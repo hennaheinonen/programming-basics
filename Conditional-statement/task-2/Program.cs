1473b4f86db32d66b671445068a6dd9ad277cf28
hennaheinonen <henna.heinonen96@gmail.com>
2017-09-22 14:22:38 +0300

    task2

---
+++ Conditional-statement/task-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Define instructions
+            Console.WriteLine("Ohjelma selvittää onko syötetty luku pariton vai parillinen");
+            bool isNumber;
+            do
+            {
+                Console.WriteLine("Syötä luku: ");
+                string userInput;
+                userInput = Console.ReadLine();
+
+                int.TryParse(userInput, out int evaluatedNumber);
+                isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+
+                // program logic
+                if (evaluatedNumber % 2 == 0)
+                {
+                    Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on parillinen");
+                }
+                else
+                {
+                    Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on pariton");
+                }
+
+            } while (isNumber == false);
+            Console.ReadKey();
+    }
+    }
+}
+
+
+
+    
+
