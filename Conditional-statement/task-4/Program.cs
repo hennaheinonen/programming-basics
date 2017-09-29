d85b87e8b1041245f003d3e6ea0370caf94eeaf8
hennaheinonen <henna.heinonen96@gmail.com>
2017-09-29 10:52:35 +0300

    task-4

--- Conditional-statement/task-4/Program.cs
+++ Conditional-statement/task-4/Program.cs
@@ -6,7 +6,65 @@ namespace task4
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Define instructions
+
+            Console.WriteLine("Ohjemlma asettaa luvut suuruusjärjestykseen");
+
+            Console.WriteLine("Syötä Numero 1:");
+            string numero1 = Console.ReadLine();
+            int numx = int.Parse(numero1);
+
+            Console.WriteLine("Syötä Numero 2:");
+            string numero2 = Console.ReadLine();
+            int numy = int.Parse(numero2);
+
+            Console.WriteLine("Syötä Numero 3:");
+            string numero3 = Console.ReadLine();
+            int numz = int.Parse(numero3);
+
+            if (numx < numy)
+            {
+                if (numx < numz)
+                {
+                    if (numy < numz)
+                    {
+                        Console.WriteLine($"järjestys on : {numx}, {numy}, {numz}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"järjestys on : {numz}, {numy}, {numx}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"järjestys on : {numz}, {numx}, {numy}");
+                }
+
+            }
+            else
+            {
+                if (numx < numz)
+                {
+                    if (numy < numz) 
+                    {
+                        Console.WriteLine($"järjestys on : {numy}, {numx}, {numz}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"järjestys on : {numy}, {numz}, {numx}");
+                    }    
+                }
+                else 
+                {
+                    Console.WriteLine($"järjestys on : {numz}, {numy}, {numx}");
+                }    
+            }    
+ 
+
+
+
+            Console.WriteLine($"{numx}, {numy}, {numz}");
+			Console.ReadKey();
         }
     }
 }
