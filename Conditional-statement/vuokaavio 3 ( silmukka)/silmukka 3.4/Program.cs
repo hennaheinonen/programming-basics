43a49a31f80323041581d69ef7b0a2a6c8596b9d
hennaheinonen <henna.heinonen96@gmail.com>
2017-10-13 11:25:01 +0300

    Task-3.4
    Silmukka 3.4

---
+++ Conditional-statement/vuokaavio 3 ( silmukka)/silmukka 3.4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write( "Ohjelma laskee ensimmäistä lukua yhteen. Anna luku, jonka yhteenlasku lasketaan");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            //int.TryParse(userInput, out int number);
+            int i = 0;
+            long f = 0;
+            if (number > 0)
+			{
+				do
+				{
+					i = i + 1;
+					f = f + i;
+					//Console.WriteLine($"Luvun {number} yhteenlaskettu tulos on {f}");
+				} while (i < number);
+				Console.WriteLine("Syötit: " + number);
+				Console.WriteLine($"Vastaus: {f}");
+			}
+			else
+			{
+				do
+				{
+					i = i - 1;
+					f = f + i;
+					//Console.WriteLine($"Luvun {number} yhteenlaskettu tulos on {f}");
+				} while (i > number);
+				Console.WriteLine("Syötit: " + number);
+				Console.WriteLine($"Vastaus: {f}");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
+
