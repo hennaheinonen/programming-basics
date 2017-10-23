cdfd4784e6d31bcc5123629eef6394dacbfa8081
hennaheinonen <henna.heinonen96@gmail.com>
2017-10-23 12:52:02 +0300

    Task-4.5
    Task-4.5

---
+++ Conditional-statement/task-4.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa vakioveikkauksen rivin");
+            Random rnd = new Random();
+            for (int i = 1; i <= 13; i++)
+            {
+                double j = rnd.NextDouble();
+
+                if (j <= 0.4)
+                {
+                    Console.WriteLine($"{i}. 1");
+                }
+                else if (j >= 0.8)
+                {
+                    Console.WriteLine($"{i}. x");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}. 2");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
