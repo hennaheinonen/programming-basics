0fc0f0ec16fe6114a03e26c2b50b34a1370fe864
hennaheinonen <henna.heinonen96@gmail.com>
2017-10-23 11:09:02 +0300

    Task-4.4
    task-4.4

---
+++ Conditional-statement/task-4.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma simuloi rahan heittoa");
+            Random rnd = new Random();
+            string UserInput = Console.ReadLine();
+            int numero = int.Parse(UserInput);
+            int kruuna = 0;
+            int klaava = 0;
+
+            Console.WriteLine($"Rahaa on heitetty {numero} kertaa");
+
+            for (int i = 0; i < numero; i++)
+            {
+                if (rnd.Next(2) == 0)
+                    kruuna++;
+                else
+                    klaava++;
+            }    
+
+            Console.WriteLine($"Klaavoja tuli {klaava} ja kruunia {kruuna}");
+
+            Console.ReadKey();
+        }   
+    }
+}
