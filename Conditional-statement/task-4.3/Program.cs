beb645174a33e6d032501b3b05a08a3709e50592
hennaheinonen <henna.heinonen96@gmail.com>
2017-10-23 10:32:46 +0300

    Task-4.3
    task-4.3

---
+++ Conditional-statement/task-4.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua");
+
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write($"Rivi {i}: ");
+                for (int y = 1; y <= 5; y++)
+                {
+                    if (y < 5)
+                        Console.Write($"{rnd.Next(50)},");
+                    else
+                        Console.Write($"{rnd.Next(50)} \n");
+                }    
+            }    
+            Console.ReadKey();
+        }
+
+    }
+}
