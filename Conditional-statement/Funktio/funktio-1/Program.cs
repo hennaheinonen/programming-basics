ed42fa0aff3071c0063642879095f510549debd8
hennaheinonen <henna.heinonen96@gmail.com>
2017-10-24 12:08:45 +0300

    Funktio-1
    Funktio-1

---
+++ Conditional-statement/Funktio/funktio-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace funktio1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Anna luku");
+            int numStars = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{GetStars(numStars)}");
+        }
+
+        static string GetStars(int numStars)
+        {
+            string printStars = "";
+
+            do
+            {
+                if (numStars <= 0)
+                {
+                    Console.WriteLine("Luku on virheellinen. Anna uusi luku.");
+                    numStars = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (true);
+
+            for (int i = 0; i < numStars; i++)
+            {
+                printStars += "*"; 
+            }
+            return printStars;
+
+        }
+    }
+}
