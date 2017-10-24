3822c93ccbb8cf181cb9faa0d0302286a1a1057b
hennaheinonen <henna.heinonen96@gmail.com>
2017-10-24 10:27:22 +0300

    task-4.6
    Task-4.6

---
+++ Conditional-statement/task-4.6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("Ohjelma simuloi nopanheittoa");
+			Random rnd = new Random();
+			int sixCounter = 0;
+
+			for (int i = 1; i <= 1000; i++)
+			{
+				int j = rnd.Next(1,7);
+
+				if (j == 6)
+				{
+					sixCounter++;
+				}
+				Console.WriteLine($"{i}.\t{j}");
+			}
+			Console.WriteLine($"Kuutonen arvottiin {sixCounter} kertaa.");
+			Console.ReadKey();
+        }
+    }
+}
