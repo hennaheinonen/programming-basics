1154d2891bc27c71392e66d5a1f10a84537d8d0b
hennaheinonen <henna.heinonen96@gmail.com>
2017-11-16 20:05:07 +0200

    task-4.2
    task-4.2

---
+++ Conditional-statement/task-4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9.");
+            int summa;
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int y = 1; y <= 9;y++)
+                {
+                    summa = i * y;
+                    Console.WriteLine($"{i} * {y} = {summa} ");
+                }    
+            }
+
+        }
+    }
+}
