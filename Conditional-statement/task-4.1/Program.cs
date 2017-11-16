b99960cb153c7d958c9ceaf7472d9c4a2e433ade
hennaheinonen <henna.heinonen96@gmail.com>
2017-11-16 20:04:19 +0200

    task-4.1
    task-4.1

---
+++ Conditional-statement/task-4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("Ohjelma tulostaa kokonaislukujen 1-10 neliöjuuret");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} {Math.Sqrt(i)}");
+            }      
+
+        }
+    }
+}
